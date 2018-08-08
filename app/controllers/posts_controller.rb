0cbb20c341865b6595ecf310f532bf1002d46e5c
Jayon Kim <jpkim921@gmail.com>
2018-08-08 23:44:12 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+
+    # we can delete the line below since we're calling the local variable when using partials
     @author = @post.author
   end
 
